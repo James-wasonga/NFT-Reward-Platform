2b0f9cb922ba6424c2117a7f505466ee6c82db3b
James-wasonga <jameswasonga22@gmail.com>
2025-06-03 00:38:09 +0300

    create file

---
+++ frontend/src/constants.js
@@ -0,0 +1,3 @@
+// src/constants.js
+export const NFT_CONTRACT_ADDRESS = import.meta.env.VITE_NFT_CONTRACT_ADDRESS;
+export const TOKEN_CONTRACT_ADDRESS = import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS;
